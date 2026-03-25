e419fe4f36047292e87ee91fbc850ef51ef70e91
gonsalvesbliss <blissgonsalves07@gmail.com>
2026-03-26 00:24:17 +0530

    Refactor Cart component structure

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,13 +1,10 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
-
 function Cart() {
   const { cart, removeFromCart } = useContext(CartContext);
-
   return (
     <div>
       <h2>Your Cart</h2>
-
       {cart.length === 0 ? (
         <p>Cart is empty</p>
       ) : (
@@ -15,7 +12,6 @@ function Cart() {
           <div key={item.id} style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
             <h4>{item.name}</h4>
             <p>Price: {item.price}</p>
-
             <button onClick={() => removeFromCart(item.id)}>
               Remove
             </button>
@@ -25,5 +21,4 @@ function Cart() {
     </div>
   );
 }
-
 export default Cart;
